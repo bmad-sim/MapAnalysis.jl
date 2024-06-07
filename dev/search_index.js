170d5a6180fa7467ed988efd1c6ebf1f201502a4
Documenter.jl <documenter@juliadocs.github.io>
2024-06-07 16:04:30 +0000

    build based on a4a88bc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MapAnalysis","category":"page"},{"location":"#MapAnalysis","page":"Home","title":"MapAnalysis","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MapAnalysis.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MapAnalysis]","category":"page"}]
+}
